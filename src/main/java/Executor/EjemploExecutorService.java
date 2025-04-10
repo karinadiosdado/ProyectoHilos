a9a5a5285620d7d8aeec15ddc8bcda9452d0b71e
Karina Diosdado <l.dimk050419@itses.edu.mx>
2025-04-10 09:56:59 -0600

    el push de la fecha 10/04/2025

---
+++ src/main/java/Executor/EjemploExecutorService.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Executor;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author karin
+ */
+public class EjemploExecutorService {// este no nececita iniciarlo solo serrarlo 
+    public static void main(String[] args) {
+ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);    
+        System.out.println("Tarea del main");
+        executor.schedule(()->{
+            System.out.println("Inicio de la tarea");
+            try {
+                System.out.println("");
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+                e.printStackTrace();
+            }
+            
+        }, 2, TimeUnit.SECONDS);
+        System.out.println("Hasta aqui llegue");
+        executor.shutdown();
+        System.out.println("Hola sigo voivo ");
+    }
+}
